f0f91a894f1f7ff8f273c40fa922103239700eb8
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-04-29 23:09:20 -0300

    colocando valores de ataque e defesa nas cartas

--- jogo/models.py
+++ jogo/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Carta(models.Model):
     front = models.ImageField(upload_to= 'imagem', blank=False)
     elemento = models.CharField(max_length= 100,  blank=False, default='')
+    ataque = models.PositiveIntegerField(blank=False, default=0)
+    defesa = models.PositiveIntegerField(blank=False, default=0)
