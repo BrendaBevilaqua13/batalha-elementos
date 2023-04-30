f69a34040242d41ceebb00eff16727a6f4009cea
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-04-30 18:02:56 -0300

    construindo os turnos

--- jogo/views.py
+++ jogo/views.py
@@ -3,35 +3,56 @@ from .models import Carta
 import random
 
 
+baralho_maquina = []
+baralho_user = []
+nome = ''
+
+
 def batalha(request):
+    global nome
+    global baralho_maquina
+    global baralho_user
+    global embaralhar
     if request.method == 'POST':
         nome = request.POST['nome'].strip()
 
         if len(nome) == 0 or len(nome) > 8:
             return render(request, 'index.html')
-      
+        
         cartas = Carta.objects.all()
-        baralho = []
-        while len(baralho) < 5:
-            elemento = random.choice(cartas)
-            if elemento in baralho:
-                continue
-            baralho.append(elemento)  
 
+        while len(baralho_maquina) < 5:
+            elemento = random.choice(cartas)
+            if elemento in baralho_maquina:
+                continue
+            baralho_maquina.append(elemento)
+            
+        while len(baralho_user)< 5:
+            elemento2 = random.choice(cartas)
+            if elemento2 in baralho_user:
+                continue
+            baralho_user.append(elemento2) 
+              
         context = {
             'nome': nome.strip().capitalize(),
-            'baralho': baralho,
+            'baralho_maquina': baralho_maquina,
+            'baralho_user': baralho_user,
         }
-        
         return render(request, 'batalha.html', context)
 
            
 
 def teste(request, id):
+    global nome
+    global baralho_maquina
+    global baralho_user
     
     carta = get_object_or_404(Carta,pk=id)
     context = {
-       'carta': carta,
+        'carta': carta,
+        'nome': nome,
+        'baralho_maquina': baralho_maquina,
+        'baralho_user': baralho_user,
     }
     return render(request, 'batalha.html', context)
             
